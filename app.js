7141fc47fa1b42f65043baa3affd8e6f4836be76
christianJimenez1999 <chrjimenez@csumb.edu>
2020-04-06 06:10:20 +0000

    up

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 const express = require("express");
 const app = express();
+var bodyparser = require('body-parser')
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
+const chalk = require('chalk');
+
 
 //routes
 app.get("/", function(req, res){
-   res.render("index.html"); 
+  console.log(chalk.blue('render succeeded') + chalk.red('!'));
+  res.render("index.ejs"); 
 });
 
-// app.get("/c++", function(req, res){
-//    res.render("C++.html"); 
-// });
+app.get("/c", function(req, res){
+    console.log(chalk.blue('render succeeded') + chalk.red('!'));
+    res.render("C++.ejs"); 
+});
 
-// app.get("/java", function(req, res) {
-//    res.render("Java.html"); 
-// });
+app.get("/java", function(req, res) {
+    console.log(chalk.blue('render succeeded') + chalk.red('!'));
+    res.render("Java.ejs"); 
+});
 
-// app.get("/python", function(req, res){
-//    res.render("Python.html"); 
-// });
+app.get("/python", function(req, res){
+    console.log(chalk.blue('render succeeded') + chalk.red('!'));
+    res.render("Python.ejs"); 
+});
 
-app.listen("8080", "127.0.0.1", function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Running Express Server...");
 });
